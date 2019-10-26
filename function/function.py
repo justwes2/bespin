50158c5c6c301cb649419bccca218a819a138869
wes <wes.coffay@gmail.com>
2019-10-25 22:38:03 -0400

    added test function zip

---
+++ function/function.py
@@ -0,0 +1,10 @@
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'headers' {
+            'Content-Type': 'text/html; charset=utf-8'
+        },
+        'body': '<p>Hello World</p>'
+    }
