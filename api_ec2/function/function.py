e198b78d8c2fecf5b9eef5d026b3a666826fec96
wes <wes.coffay@gmail.com>
2019-10-27 17:29:08 -0400

    updated api function

--- api_ec2/function/function.py
+++ api_ec2/function/function.py
@@ -1,10 +1,19 @@
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
+
+    dynamo_resource = boto3.resource('dynamodb')
+    table = dynamo_resource.Table('bespin_report_ec2')
+
+    response = table.scan()
+    instance_data = response['Items']
+    
     return {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/html; charset=utf-8'
+            'Content-Type': 'application/json'
         },
-        'body': '<p>Bonjour au monde</p>'
+        'body': instance_data
     }
+
+lambda_handler("", "")
